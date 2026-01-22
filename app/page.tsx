c40cb1374c8343929f437291af3c57de9c5a6f91
Anshpreet Singh <anshpreet.singh@edu.sait.ca>
2026-01-22 16:45:14 -0700

    Refactor Home component to display assignments
    Removed unused components and added assignment links.

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,19 @@
-import Image from "next/image";
-import Header from "../components/header";
-import Calculate, { calculateSomething } from "@/components/calculate";
+import Link from "next/link";
 
 export default function Home() {
-  const results = calculateSomething();
   return (
-    <div className="flex min-h-screen items-center justify-center bg-red-50 font-sans dark:bg-black w-full">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-start px-16 bg-white dark:bg-black sm:items-start">
-        <Header />
-        <div className="py-12">
-          <Image
-            className="dark:invert rounded-2xl shadow-lg border border-red-500"
-            src="/teddy_bear.jpg"
-            alt="Next.js logo"
-            width={200}
-            height={200}
-            priority
-          />
-          <Calculate />
-          <div>{results.sum}</div>
-        </div>
-      </main>
-    </div>
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+
+      <ul>
+        <li>
+          <Link href="/week-2">Week 2 Assignment</Link>
+        </li>
+      </ul>
+
+      <p>
+        This site contains all weekly assignments for CPRG 306.
+      </p>
+    </main>
   );
 }
