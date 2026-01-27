069f7dc47c0b3d917310452e6813e4ed204e420b
Anshpreet Singh <anshpreet.singh@edu.sait.ca>
2026-01-27 14:00:51 -0700

    Update page components

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">CPRG 306 Assignments</h1>
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
 
-      <ul className="list-disc pl-6 space-y-2">
-        <li>
-          <Link href="/week-2" className="text-blue-600 hover:underline">
-            Week 2
-          </Link>
-        </li>
-
-        <li>
-          <Link href="/week-3" className="text-blue-600 hover:underline">
-            Week 3
-          </Link>
-        </li>
-      </ul>
+      <Link href="/week-2">Week 2 Assignment</Link>
+      <br />
+      <Link href="/week-3">Week 3 Shopping List</Link>
     </main>
   );
+
 }
