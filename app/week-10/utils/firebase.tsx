02802c74ac9b5f4b4f4177c68912c2e34b21417a
anshpreetsingh007 <anshpreet.singh@edu.sait.ca>
2026-03-24 15:01:30 -0600

    fixes

--- app/week-10/utils/firebase.tsx
+++ app/week-10/utils/firebase.tsx
@@ -1,17 +1,17 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQeUv5sfTaBBvjWHoisBIkj-9rjDtHNG8",
-  authDomain: "cprg306-assignments-aad71.firebaseapp.com",
-  projectId: "cprg306-assignments-aad71",
-  storageBucket: "cprg306-assignments-aad71.firebasestorage.app",
-  messagingSenderId: "603749182032",
-  appId: "1:603749182032:web:f7859e8f5e83fe84a3bf55",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
